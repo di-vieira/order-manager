f59e104b42b3ae3c15b18e51712ee9e57a6212ef
Diego Vieira <diegodasilvavieira@gmail.com>
2023-07-22 01:08:35 +0200

    Project created

---
+++ src/main/java/com/diego/study/ordermanager/OrderManagerApplication.java
@@ -0,0 +1,14 @@
+package com.diego.study.ordermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diego/study/ordermanager/OrderManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diego.study.ordermanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
