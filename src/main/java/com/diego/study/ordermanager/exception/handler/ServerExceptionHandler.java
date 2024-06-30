0dd48fa9d3453753841d9947f4f0075b17ee0047
Diego da Silva Vieira <dvieira@ravemobilesafety.com>
2024-06-30 14:07:49 +0200

    Removed not used imports from ServerExceptionHandler

--- src/main/java/com/diego/study/ordermanager/exception/handler/ServerExceptionHandler.java
+++ src/main/java/com/diego/study/ordermanager/exception/handler/ServerExceptionHandler.java
@@ -1,14 +1,11 @@
 package com.diego.study.ordermanager.exception.handler;
 
-import com.diego.study.ordermanager.exception.BusinessException;
-import com.diego.study.ordermanager.exception.response.BusinessExceptionMessage;
 import com.diego.study.ordermanager.exception.response.InternalServerErrorMessage;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.client.HttpServerErrorException;
 
 import java.time.LocalDateTime;
 
